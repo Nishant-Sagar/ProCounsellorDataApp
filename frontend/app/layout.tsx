c574a45633f6a59b7cb673122af44ab80bac10fe
Ashutosh kumar <96118879+ashutosh7856@users.noreply.github.com>
2025-07-18 17:04:02 +0000

    Metadata added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,11 @@
 import Footer from "@/components/Footer";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Metadata } from "next";
+export const metadata :Metadata={
+  title:"ProCounsel",
+  description:"ProCounsel platform for students to connect with counsellors in the neighborhood."
+}
 export default function RootLayout({
   children,
 }: {
