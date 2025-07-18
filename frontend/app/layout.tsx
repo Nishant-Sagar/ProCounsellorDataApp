4d125d02eeb4b7c5b53b59045c3f82cf6c21fda6
Ashutosh kumar <96118879+ashutosh7856@users.noreply.github.com>
2025-07-18 16:41:19 +0000

    Header and footer components added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,6 @@
+import Footer from "@/components/Footer";
 import "./globals.css";
+import Header from "@/components/Header";
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Header/>
       <body>{children}</body>
+      <Footer/>
     </html>
   );
 }
